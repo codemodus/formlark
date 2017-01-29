72934c59f5e11142ce8fdd0167426d2a3ec21864
Daved <daved@codemodus.com>
2017-01-29 13:02:56 -0800

    Redo.

--- front/assets/app/pipes/pipes.js
+++
@@ -1,6 +0,0 @@
-import { defaultPipes } from 'angular2/change_detection';
-import { CurrencyPipe } from './currency';
-
-export var pipes = Object.assign({}, defaultPipes, {
-  'currency': [new CurrencyPipe()]
-});
